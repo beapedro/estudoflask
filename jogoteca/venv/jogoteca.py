539332f2d317c94f8068043e7220fa0b39ced74b
pedrobeako <reis83pedroaugusto@gmail.com>
2024-09-29 23:43:03 -0300

    Ariana grande

--- jogoteca/venv/jogoteca.py
+++ jogoteca/venv/jogoteca.py
@@ -1,40 +1,15 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
 
-
-class Jogo:
- 
-   def __init__(self, nome, categoria, console):
-       self.nome=nome
-       self.categoria=categoria
-       self.console=console     
 app = Flask(__name__)
 
-jogo1= Jogo('Tetris', 'Puzzle', 'Atari')
-jogo2= Jogo('Naruto', 'Lutinha', 'Ps2')
-jogo3= Jogo('Lol','Moba','PC')
-lista =  [jogo1,jogo2,jogo3]
+app.config.from_pyfile('config.py')
 
-@app.route('/')
-def index():
-   
+db = SQLAlchemy(app)
 
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
+from views import *
 
-
-@app.route('/novo')
-def novo():
-    return render_template('novo.html', titulo='Novo Jogo')
-
-
-@app.route('/criar', methods=['POST',])
-def criar():
-   
-   nome = request.form['nome']
-   categoria = request.form['categoria']
-   console = request.form['console']
-
-   jogo = Jogo(nome, categoria, console)
-   lista.append(jogo)
-   return redirect('/')
-
-app.run()
+if __name__ == '__main__':
+    app.run(DEBUG = True)
+    
+ 
